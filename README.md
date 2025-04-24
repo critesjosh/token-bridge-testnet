81de736ec96425420ab5a43dc2a6147368c0e093
Josh Crites <jc@joshcrites.com>
2025-04-23 22:10:51 -0400

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+# Token bridge tutorial on testnet
+
+Running PXE:
+
+```bash
+aztec start --port 8081 --pxe --pxe.nodeUrl=http://34.107.66.170/ --pxe.proverEnabled true --l1-chain-id 11155111
+```
+
+Explorer url: https://aztecscan.xyz/tx-effects/0x0097e9e242d043591ce0ab42be7f3e280a28b0569556191e5b4e11def8912ff1
+
+Error
+
+```bash
+[21:41:52.778] INFO: token-bridge-tutorial Schnorr account deployed at: 0x1eb084349662e678f7feeed902a1378db541cf1a1b1fb69c4d1c68cacce65ae6
+[21:41:54.938] INFO: token-bridge-tutorial L1 Contract Addresses:
+[21:41:54.939] INFO: token-bridge-tutorial Registry Address: 0x4d2cc1d5fb6be65240e0bfc8154243e69c0fb19e
+[21:41:54.939] INFO: token-bridge-tutorial Inbox Address: 0x812bdd3aa323fe28db926b55d7f0273b3efadf06
+[21:41:54.939] INFO: token-bridge-tutorial Outbox Address: 0x4345f34cc1a4be57156f1d0c669f9f5cbfb15d0a
+[21:41:54.939] INFO: token-bridge-tutorial Rollup Address: 0x8d1cc702453fa889f137dbd5734cdb7ee96b6ba0
+[21:46:10.963] INFO: aztecjs:deploy_sent_tx Contract 0x0c6d6aa66f4bd520a9274b3d4587da8b411824aa43113e9bf9034b3faf87818c successfully deployed.
+[21:46:10.963] INFO: token-bridge-tutorial L2 token contract deployed at 0x0c6d6aa66f4bd520a9274b3d4587da8b411824aa43113e9bf9034b3faf87818c
+[21:46:54.746] INFO: token-bridge-tutorial erc20 contract deployed
+[21:47:41.851] INFO: token-bridge-tutorial L1 portal contract deployed
+[21:51:56.864] INFO: aztecjs:deploy_sent_tx Contract 0x2f043db051f7b9cf7f512fe6306370ee9979da9cc597637a1dc0af76afe495d7 successfully deployed.
+[21:51:56.864] INFO: token-bridge-tutorial L2 token bridge contract deployed at 0x2f043db051f7b9cf7f512fe6306370ee9979da9cc597637a1dc0af76afe495d7
+[21:54:03.926] INFO: token-bridge-tutorial L1 portal contract initialized
+[21:54:07.951] INFO: token-bridge-tutorial Minting 1000000000000000 tokens for 0xB84eb0bfCc25C832acad39D315a3B7B0e959e252
+[21:54:16.933] INFO: token-bridge-tutorial Approving 1000000000000000 tokens for TokenPortal (0x78a4eab470a15970060ebc4b1e0f97487abb7273)
+[21:54:29.123] INFO: token-bridge-tutorial Sending L1 tokens to L2 to be claimed publicly
+[22:01:45.111] INFO: token-bridge-tutorial Public L2 balance of 0x1eb084349662e678f7feeed902a1378db541cf1a1b1fb69c4d1c68cacce65ae6 is 1000000000000000
+22:05:53.235] INFO: token-bridge-tutorial New L2 balance of 0x1eb084349662e678f7feeed902a1378db541cf1a1b1fb69c4d1c68cacce65ae6 is 999999999999991
+[22:05:54.217] INFO: token-bridge-tutorial Sending L1 tx to consume message at block 9823 index 0 to withdraw 9
+/home/josh/Documents/test/token-bridge-tutorial/node_modules/@aztec/ethereum/node_modules/viem/utils/errors/getContractError.ts:78
+  return new ContractFunctionExecutionError(cause as BaseError, {
+         ^
+
+
+ContractFunctionExecutionError: The contract function "withdraw" reverted.
+
+Error: Outbox__BlockNotProven(uint256 l2BlockNumber)
+                             (9823)
+ 
+Contract Call:
+  address:   0x78a4eab470a15970060ebc4b1e0f97487abb7273
+  function:  withdraw(address _recipient, uint256 _amount, bool _withCaller, uint256 _l2BlockNumber, uint256 _leafIndex, bytes32[] _path)
+  args:              (0xb84eb0bfcc25c832acad39d315a3b7b0e959e252, 9, false, 9823, 0, ["0x0000000000000000000000000000000000000000000000000000000000000000","0x00f5a5fd42d16a20302798ef6ed309979b43003d2320d9f0e8ea9831a92759fb","0x0007638bb56b6dda2b64b8f76841114ac3a87a1820030e2e16772c4d294879c3"])
+  sender:    0xB84eb0bfCc25C832acad39D315a3B7B0e959e252
+
+Docs: https://viem.sh/docs/contract/simulateContract
+Version: viem@2.23.7
+```
---
+++ index.ts
@@ -0,0 +1,318 @@
+import { getInitialTestAccountsWallets } from "@aztec/accounts/testing";
+import {
+  EthAddress,
+  Fr,
+  L1TokenManager,
+  L1TokenPortalManager,
+  createLogger,
+  createPXEClient,
+  waitForPXE,
+} from "@aztec/aztec.js";
+import { createL1Clients, deployL1Contract } from "@aztec/ethereum";
+import { deriveSigningKey } from '@aztec/stdlib/keys';
+import {
+  FeeAssetHandlerAbi,
+  FeeAssetHandlerBytecode,
+  TestERC20Abi,
+  TestERC20Bytecode,
+  TokenPortalAbi,
+  TokenPortalBytecode,
+} from "@aztec/l1-artifacts";
+import { TokenContract } from "@aztec/noir-contracts.js/Token";
+import { TokenBridgeContract } from "@aztec/noir-contracts.js/TokenBridge";
+import { SponsoredFeePaymentMethod } from "@aztec/aztec.js/fee/testing";
+
+import { getContract } from "viem";
+
+import {
+    type ContractInstanceWithAddress,
+    type PXE,
+    getContractInstanceFromDeployParams,
+  } from '@aztec/aztec.js';
+  import { SponsoredFPCContract } from '@aztec/noir-contracts.js/SponsoredFPC';
+import { getSchnorrAccount } from "@aztec/accounts/schnorr";
+import 'dotenv/config';
+import { sepolia } from "viem/chains";
+
+const SPONSORED_FPC_SALT = new Fr(0);
+
+export async function getSponsoredFPCInstance(): Promise<ContractInstanceWithAddress> {
+  return await getContractInstanceFromDeployParams(SponsoredFPCContract.artifact, {
+    salt: SPONSORED_FPC_SALT,
+  });
+}
+
+export async function getSponsoredFPCAddress() {
+  return (await getSponsoredFPCInstance()).address;
+}
+
+export async function getDeployedSponsoredFPCAddress(pxe: PXE) {
+  const fpc = await getSponsoredFPCAddress();
+  const contracts = await pxe.getContracts();
+  if (!contracts.find(c => c.equals(fpc))) {
+    throw new Error('SponsoredFPC not deployed.');
+  }
+  return fpc;
+}
+
+const { ETHEREUM_HOSTS = "https://ethereum-sepolia-rpc.publicnode.com", PRIVATE_KEY } = process.env;
+
+if (!PRIVATE_KEY) {
+  throw new Error('PRIVATE_KEY must be set in .env file');
+}
+
+const { walletClient, publicClient } = createL1Clients(
+  ETHEREUM_HOSTS.split(","),
+  PRIVATE_KEY,
+  // @ts-ignore
+  sepolia
+);
+const ownerEthAddress = walletClient.account.address;
+
+const MINT_AMOUNT = BigInt(1e15);
+
+const setupSandbox = async () => {
+  const { PXE_URL = "http://localhost:8081" } = process.env;
+  // eslint-disable-next-line @typescript-eslint/await-thenable
+  const pxe = await createPXEClient(PXE_URL);
+  await waitForPXE(pxe);
+  return pxe;
+};
+
+async function deployTestERC20(): Promise<EthAddress> {
+  const constructorArgs = ["Test Token", "TEST", walletClient.account.address];
+
+  return await deployL1Contract(
+    walletClient,
+    publicClient,
+    TestERC20Abi,
+    TestERC20Bytecode,
+    constructorArgs
+  ).then(({ address }) => address);
+}
+
+async function deployFeeAssetHandler(
+  l1TokenContract: EthAddress
+): Promise<EthAddress> {
+  const constructorArgs = [
+    walletClient.account.address,
+    l1TokenContract.toString(),
+    MINT_AMOUNT,
+  ];
+  return await deployL1Contract(
+    walletClient,
+    publicClient,
+    FeeAssetHandlerAbi,
+    FeeAssetHandlerBytecode,
+    constructorArgs
+  ).then(({ address }) => address);
+}
+
+async function deployTokenPortal(): Promise<EthAddress> {
+  return await deployL1Contract(
+    walletClient,
+    publicClient,
+    TokenPortalAbi,
+    TokenPortalBytecode,
+    []
+  ).then(({ address }) => address);
+}
+
+async function addMinter(
+  l1TokenContract: EthAddress,
+  l1TokenHandler: EthAddress
+) {
+  const contract = getContract({
+    address: l1TokenContract.toString(),
+    abi: TestERC20Abi,
+    // @ts-ignore
+    client: walletClient,
+  });
+  // @ts-ignore
+  await contract.write.addMinter([l1TokenHandler.toString()]);
+}
+
+async function main() {
+  const logger = createLogger("aztec:token-bridge-tutorial");
+  const pxe = await setupSandbox();
+
+let secretKey = Fr.random();
+let salt = Fr.random();
+let schnorrAccount = await getSchnorrAccount(pxe, secretKey, deriveSigningKey(secretKey), salt);
+let ownerWallet = await schnorrAccount.getWallet();
+const ownerAztecAddress = ownerWallet.getAddress();
+const sponseredFPC = await getSponsoredFPCInstance();
+await pxe.registerContract({instance: sponseredFPC, artifact: SponsoredFPCContract.artifact});
+const paymentMethod = new SponsoredFeePaymentMethod(sponseredFPC.address);
+
+let tx = await schnorrAccount.deploy({ fee: { paymentMethod } }).wait({timeout: 120000});
+
+logger.info(`Schnorr account deployed at: ${ownerWallet.getAddress()}`);
+
+  const l1ContractAddresses = (await pxe.getNodeInfo()).l1ContractAddresses;
+  logger.info("L1 Contract Addresses:");
+  logger.info(`Registry Address: ${l1ContractAddresses.registryAddress}`);
+  logger.info(`Inbox Address: ${l1ContractAddresses.inboxAddress}`);
+  logger.info(`Outbox Address: ${l1ContractAddresses.outboxAddress}`);
+  logger.info(`Rollup Address: ${l1ContractAddresses.rollupAddress}`);
+
+  const l2TokenContract = await TokenContract.deploy(
+    ownerWallet,
+    ownerAztecAddress,
+    "L2 Token",
+    "L2",
+    18
+  )
+    .send({ fee: { paymentMethod } })
+    .deployed({timeout: 120000});
+  logger.info(`L2 token contract deployed at ${l2TokenContract.address}`);
+
+  const l1TokenContract = await deployTestERC20();
+  logger.info("erc20 contract deployed");
+
+  const feeAssetHandler = await deployFeeAssetHandler(l1TokenContract);
+  await addMinter(l1TokenContract, feeAssetHandler);
+
+  const l1TokenManager = new L1TokenManager(
+    l1TokenContract,
+    feeAssetHandler,
+    publicClient,
+    walletClient,
+    logger
+  );
+
+  const l1PortalContractAddress = await deployTokenPortal();
+  logger.info("L1 portal contract deployed");
+
+  const l1Portal = getContract({
+    address: l1PortalContractAddress.toString(),
+    abi: TokenPortalAbi,
+    // @ts-ignore
+    client: walletClient,
+  });
+
+  const l2BridgeContract = await TokenBridgeContract.deploy(
+    ownerWallet,
+    l2TokenContract.address,
+    l1PortalContractAddress
+  )
+    .send({ fee: { paymentMethod } })
+    .deployed({timeout: 120000});
+  logger.info(
+    `L2 token bridge contract deployed at ${l2BridgeContract.address}`
+  );
+
+  await l2TokenContract.methods
+    .set_minter(l2BridgeContract.address, true)
+    .send({ fee: { paymentMethod } })
+    .wait({timeout: 120000});
+
+  // @ts-ignore
+  await l1Portal.write.initialize(
+    [
+      l1ContractAddresses.registryAddress.toString(),
+      l1TokenContract.toString(),
+      l2BridgeContract.address.toString(),
+    ],
+    {}
+  );
+  logger.info("L1 portal contract initialized");
+
+  const l1PortalManager = new L1TokenPortalManager(
+    l1PortalContractAddress,
+    l1TokenContract,
+    feeAssetHandler,
+    l1ContractAddresses.outboxAddress,
+    publicClient,
+    walletClient,
+    logger
+  );
+
+  const claim = await l1PortalManager.bridgeTokensPublic(
+    ownerAztecAddress,
+    MINT_AMOUNT,
+    true
+  );
+
+  // Do 2 unrleated actions because
+  // https://github.com/AztecProtocol/aztec-packages/blob/7e9e2681e314145237f95f79ffdc95ad25a0e319/yarn-project/end-to-end/src/shared/cross_chain_test_harness.ts#L354-L355
+  await l2TokenContract.methods
+    .mint_to_public(ownerAztecAddress, 0n)
+    .send({ fee: { paymentMethod } })
+    .wait({timeout: 120000});
+  await l2TokenContract.methods
+    .mint_to_public(ownerAztecAddress, 0n)
+    .send({ fee: { paymentMethod } })
+    .wait({timeout: 120000});
+
+  await l2BridgeContract.methods
+    .claim_public(
+      ownerAztecAddress,
+      MINT_AMOUNT,
+      claim.claimSecret,
+      claim.messageLeafIndex
+    )
+    .send({ fee: { paymentMethod } })
+    .wait({timeout: 120000});
+  const balance = await l2TokenContract.methods
+    .balance_of_public(ownerAztecAddress)
+    .simulate();
+  logger.info(`Public L2 balance of ${ownerAztecAddress} is ${balance}`);
+
+  const withdrawAmount = 9n;
+  const nonce = Fr.random();
+
+  // Give approval to bridge to burn owner's funds:
+  const authwit = await ownerWallet.setPublicAuthWit(
+    {
+      caller: l2BridgeContract.address,
+      action: l2TokenContract.methods.burn_public(
+        ownerAztecAddress,
+        withdrawAmount,
+        nonce
+      ),
+    },
+    true
+  );
+
+  await authwit.send({ fee: { paymentMethod } }).wait({timeout: 120000});
+
+  const l2ToL1Message = await l1PortalManager.getL2ToL1MessageLeaf(
+    withdrawAmount,
+    EthAddress.fromString(ownerEthAddress),
+    l2BridgeContract.address,
+    EthAddress.ZERO
+  );
+  const l2TxReceipt = await l2BridgeContract.methods
+    .exit_to_l1_public(
+      EthAddress.fromString(ownerEthAddress),
+      withdrawAmount,
+      EthAddress.ZERO,
+      nonce
+    )
+    .send({ fee: { paymentMethod } })
+    .wait({timeout: 120000} );
+
+  const newL2Balance = await l2TokenContract.methods
+    .balance_of_public(ownerAztecAddress)
+    .simulate();
+  logger.info(`New L2 balance of ${ownerAztecAddress} is ${newL2Balance}`);
+
+  const [l2ToL1MessageIndex, siblingPath] =
+    await pxe.getL2ToL1MembershipWitness(
+      l2TxReceipt.blockNumber!,
+      l2ToL1Message
+    );
+  await l1PortalManager.withdrawFunds(
+    withdrawAmount,
+    EthAddress.fromString(ownerEthAddress),
+    BigInt(l2TxReceipt.blockNumber!),
+    l2ToL1MessageIndex,
+    siblingPath
+  );
+  const newL1Balance = await l1TokenManager.getL1TokenBalance(ownerEthAddress);
+  logger.info(`New L1 balance of ${ownerEthAddress} is ${newL1Balance}`);
+}
+
+main();
+
